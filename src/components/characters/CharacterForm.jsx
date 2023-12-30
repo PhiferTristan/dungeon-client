e9f4406a8debb0dfdb494d747b84dcd4d4dfcc5c
PhiferTristan <phifer.tristan@outlook.com>
2023-12-30 16:52:42 -0600

    added bond and ability managers with fetch calls and updated CharacterForm to allow users to select a bond after selecting a background and to allow users to input ability scores

--- src/components/characters/CharacterForm.jsx
+++ src/components/characters/CharacterForm.jsx
@@ -5,12 +5,18 @@ import { createCharacter } from "../../managers/CharacterManager";
 import { getAllRaces } from "../../managers/RaceManager";
 import { getAllAlignments } from "../../managers/AlignmentManager";
 import { getAllBackgrounds } from "../../managers/BackgroundManager";
+import { getBondsByBackgroundId } from "../../managers/BondManager";
+import { getAllAbilities } from "../../managers/AbilityManager";
 
 export const CharacterForm = ({ token }) => {
   const [newCharacter, setNewCharacter] = useState({});
   const [races, setRaces] = useState([]);
   const [alignments, setAlignments] = useState([]);
   const [backgrounds, setBackgrounds] = useState([]);
+  const [bonds, setBonds] = useState([]);
+  const [selectedBackground, setSelectedBackground] = useState(0);
+  const [abilities, setAbilities] = useState([]);
+  const [abilityScores, setAbilityScores] = useState({});
 
   const navigate = useNavigate();
 
@@ -26,12 +32,38 @@ export const CharacterForm = ({ token }) => {
     getAllBackgrounds(token).then((backgroundsArray) => {
       setBackgrounds(backgroundsArray);
     });
+
+    getAllAbilities(token).then((abilitiesArray) => {
+      setAbilities(abilitiesArray);
+    });
   }, [token]);
+
+  // Fetch bonds for the selected background
+  useEffect(() => {
+    if (selectedBackground !== 0) {
+      getBondsByBackgroundId(token, selectedBackground).then((bondsArray) => {
+        setBonds(bondsArray);
+        console.log(bonds);
+      });
+    }
+  }, [token, selectedBackground]);
 
   const changeCharacterState = (e) => {
     setNewCharacter({
       ...newCharacter,
       [e.target.name]: e.target.value,
+    });
+
+    // If the selected field is background, update selectedBackground
+    if (e.target.name === "background_id") {
+      setSelectedBackground(e.target.value);
+    }
+  };
+
+  const changeAbilityScore = (abilityId, score) => {
+    setAbilityScores({
+      ...abilityScores,
+      [abilityId]: score,
     });
   };
 
@@ -48,12 +80,17 @@ export const CharacterForm = ({ token }) => {
       bio: newCharacter.bio,
       character_appearance: newCharacter.character_appearance,
       notes: newCharacter.notes,
+      bond_id: parseInt(newCharacter.bond_id),
+      ability_scores: abilityScores,
     };
 
     createCharacter(token, character).then(() => {
       navigate(`/characters/mine`);
     });
   };
+
+  console.log("Bonds State:", bonds);
+  console.log("New Character State:", newCharacter);
 
   CharacterForm.propTypes = {
     token: PropTypes.string,
@@ -180,6 +217,34 @@ export const CharacterForm = ({ token }) => {
               </div>
             </div>
           </fieldset>
+          {/* Abilities Section */}
+          <div className="mb-2">
+            <label className="block text-sm font-medium text-gray-600">
+              Ability Scores:
+            </label>
+            <div className="grid grid-cols-4 gap-4">
+              {abilities.map((ability) => (
+                <div key={ability.id}>
+                  <label className="text-sm font-medium text-gray-600">
+                    {ability.label}:
+                  </label>
+                  <input
+                    type="number"
+                    min="1"
+                    max="20"
+                    value={abilityScores[ability.id] || ""}
+                    onChange={(e) =>
+                      changeAbilityScore(
+                        ability.id,
+                        parseInt(e.target.value, 10)
+                      )
+                    }
+                    className="input mt-1 p-2 border rounded-md w-full"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
           {/* Background Selection */}
           <fieldset className="field">
             <label className="label">Background: </label>
@@ -200,6 +265,33 @@ export const CharacterForm = ({ token }) => {
                       </option>
                     );
                   })}
+                </select>
+              </div>
+            </div>
+          </fieldset>
+          {/* Bond Selection */}
+          <fieldset className="field">
+            <label className="label">Bond: </label>
+            <div className="control">
+              <div className="select">
+                <select
+                  name="bond_id"
+                  value={newCharacter.bond_id}
+                  required
+                  autoFocus
+                  onChange={changeCharacterState}
+                  disabled={selectedBackground === 0} // Disable until background is selected
+                >
+                  <option value={""} disabled={selectedBackground === 0}>
+                    {selectedBackground === 0
+                      ? "Please select a background first"
+                      : "Please select a bond"}
+                  </option>
+                  {bonds.map((bond) => (
+                    <option key={bond.id} value={bond.id}>
+                      {bond.label}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
@@ -282,36 +374,3 @@ export const CharacterForm = ({ token }) => {
     </>
   );
 };
-
-{
-  /* Level Selection */
-}
-{
-  /* <fieldset className="field">
-    <label className="label">Level: </label>
-    <div className="control">
-      <div className="select">
-        <select
-          name="level"
-          value={newCharacter.level?.id}
-          required
-          autoFocus
-          onChange={changeCharacterState}
-        >
-          <option value={""}>Please select a level</option> */
-}
-{
-  /* {levels.map((level) => {
-            return (
-              <option key={level.id} value={level.id}>
-                {level.label}
-              </option>
-            );
-          })} */
-}
-{
-  /* </select>
-      </div>
-    </div>
-  </fieldset> */
-}
